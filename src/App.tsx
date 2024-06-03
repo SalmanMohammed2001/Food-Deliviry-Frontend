224910ec45e94175c422d6c8069ee5bc41bd88c9
salman <salmannisthar123@gmail.com>
2024-06-03 22:09:38 +0530

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 
 import './App.css'
 import NavBar from "./components/navbar/NavBar.tsx";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home.tsx";
+import Cart from "./pages/Cart/Cart.tsx";
+import PlaceOrder from "./pages/PlaceOrder/PlacerOrder.tsx";
 
 function App() {
 
@@ -8,6 +12,11 @@ function App() {
   return (
         <div className={'app'}>
           <NavBar/>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/cart"} element={<Cart/>}/>
+                <Route path={"/order"} element={<PlaceOrder/>}/>
+            </Routes>
         </div>
   )
 }
