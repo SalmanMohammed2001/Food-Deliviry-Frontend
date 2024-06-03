83e64f4d6bb249005733b9aa91111872ae3c3f85
salman <salmannisthar123@gmail.com>
2024-06-04 02:28:51 +0530

    FoodDispaly and FoodItem Components style add

--- src/components/FoodDisplay/FoodDisplay.tsx
+++ src/components/FoodDisplay/FoodDisplay.tsx
@@ -10,9 +10,9 @@ const FoodDisplay = ({category}) => {
 
 
     return (
-        <div className={"food-display "}>
-            <h2>Top dishes near you</h2>
-            <div className={"food-display-list"}>
+        <div className={"food-display mt-[30px]"}>
+            <h2 className={"text-2xl font-bold "}>Top dishes near you</h2>
+            <div className={"food-display-list grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[30px] mt-[30px]"}>
                 {food_list.map((item: { _id: any; name: any; price: any; description: any; image: any; }, index: Key | null | undefined) => {
                     return <FoodItem key={index} id={item._id} name={item.name} price={item.price}
                                      description={item.description} image={item.image}/>
--- src/components/FoodItem/FoodItem.tsx
+++ src/components/FoodItem/FoodItem.tsx
@@ -4,17 +4,17 @@ import {assets} from "../../assets/assets";
 // @ts-ignore
 const FoodItem=({id,name,price,description,image})=>{
     return(
-        <div className={"food-item"}>
+        <div className={"food-item w-[100%] m-auto rounded-[15px]  shadow-md"}>
             <div className={"food-item-img-container"}>
-                <img className={"food-item-img"} src={image} alt=""/>
+                <img className={"food-item-img w-[100%]  rounded-t-md  "} src={image} alt=""/>
             </div>
-            <div className={"food-item-info"}>
-                <div className={"food-item-name-rating"}>
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt=""/>
+            <div className={"food-item-info p-[20px]"}>
+                <div className={"food-item-name-rating flex justify-between items-center mb-[10px]"}>
+                    <p className={"text-[20px] font-[500]"}>{name}</p>
+                    <img className={"w-[70px]"} src={assets.rating_starts} alt=""/>
                 </div>
-                <p className={"food-item-description"}>{description}</p>
-                <p className={"food-item-price"}>${price}</p>
+                <p className={"food-item-description text-[#676767] text-[12px]"}>{description}</p>
+                <p className={"food-item-price text-orange-600 text-[22px] my-[10px] font-[500]"}>${price}</p>
             </div>
         </div>
     )
