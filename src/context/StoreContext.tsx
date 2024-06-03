d3055e643cfd88a5f57b935d5f04a179f9e4d753
salman <salmannisthar123@gmail.com>
2024-06-04 03:16:48 +0530

    StoreContext create create item useState object and addtoCart and remoecart function

--- src/context/StoreContext.tsx
+++ src/context/StoreContext.tsx
@@ -1,17 +1,41 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 // @ts-ignore
 import {food_list} from "../assets/assets";
 
-export const  StoreContext =createContext(null)
+export const StoreContext = createContext(null)
 
 // @ts-ignore
 const StoreContextProvider = ({children}) => {
 
-    const contextValue={
-            food_list
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = (itemId: string | number) => {
+
+        // @ts-ignore
+        if (!cartItems[itemId]) {
+            setCartItems((prevState) => ({...prevState, [itemId]: 1}))
+        } else {
+            // @ts-ignore
+            setCartItems((prevState) => ({...prevState, [itemId]: prevState[itemId] + 1}))
+        }
+    }
+
+    const removeFromCart = (itemId: string | number) => {
+        // @ts-ignore
+        setCartItems((prevState) => ({...prevState, [itemId]: prevState[itemId] - 1}))
+    }
+
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
+
     }
     // @ts-ignore
-    return(
+    return (
         <StoreContext.Provider value={contextValue}>
             {children}
         </StoreContext.Provider>
