295a4ba4177b0f9cdaf572bc98a0b42bd9437eca
salman <salmannisthar123@gmail.com>
2024-06-11 19:48:24 +0530

    strip configure

--- src/pages/PlaceOrder/PlacerOrder.tsx
+++ src/pages/PlaceOrder/PlacerOrder.tsx
@@ -1,5 +1,6 @@
 import {useContext, useState} from "react";
 import {StoreContext} from "../../context/StoreContext.tsx";
+import axios from "axios";
 
 
 const PlaceOrder=()=>{
@@ -42,30 +43,47 @@ const PlaceOrder=()=>{
             }
         })
 
-        console.log(orderItems)
+        let orderData={
+            address:data,
+            items:orderItems,
+            amount:getTotalAmount()+2,
+
+        }
+
+        const response= await axios.post("http://localhost:4000/api/order/place",orderData,{headers:{token}})
+
+        if(response.data.success){
+            console.log(response.data)
+
+            const{session_url} = response.data
+            window.location.replace(session_url)
+
+        }else {
+            alert("Error")
+        }
     }
 
 
     return(
         <div>
-            <form onSubmit={placeOrder} action="" className={"place-order flex items-start justify-between gap-[15px] mt-[100px]"}>
+            <form  action="" className={"place-order flex items-start justify-between gap-[15px] mt-[100px]"}>
                 <div className="placer-order-left">
                     <p className={"title  text-[30px] font-[600] mb-[50px]"}> Delivery Information</p>
                     <div className={"multi-field"}>
-                        <input type="text" placeholder={'First name'} name="firstName" onChange={onChangeHandler} value={data.firstName}/>
-                        <input type="text" placeholder={'Last name'} name="lastName" onChange={onChangeHandler} value={data.lastName}/>
+                        <input required  type="text" placeholder={'First name'} name="firstName" onChange={onChangeHandler} value={data.firstName}/>
+                        <input required  type="text" placeholder={'Last name'} name="lastName" onChange={onChangeHandler} value={data.lastName}/>
                     </div>
-                    <input type="email" placeholder={"Email"} name="email" onChange={onChangeHandler} value={data.email}/>
-                    <input type="text" placeholder={"Street"} name="street" onChange={onChangeHandler} value={data.street}/>
+                    <input required  type="email" placeholder={"Email"} name="email" onChange={onChangeHandler} value={data.email}/>
+                    <input required  type="text" placeholder={"Street"} name="street" onChange={onChangeHandler} value={data.street}/>
                     <div className={"multi-field"}>
-                        <input type="text" placeholder={'City'} name="city" onChange={onChangeHandler} value={data.city}/>
-                        <input type="text" placeholder={'State'} name="state" onChange={onChangeHandler} value={data.state}/>
+                        <input required  type="text" placeholder={'City'} name="city" onChange={onChangeHandler} value={data.city}/>
+                        <input required  type="text" placeholder={'State'} name="state" onChange={onChangeHandler} value={data.state}/>
                     </div>
                     <div className={"multi-field"}>
-                        <input type="text" placeholder={'Zip Code'} name="zipcode" onChange={onChangeHandler} value={data.zipcode}/>
-                        <input type="text" placeholder={'Country'} name="country" onChange={onChangeHandler} value={data.country}/>
+                        <input  required type="text" placeholder={'Zip Code'} name="zipcode" onChange={onChangeHandler} value={data.zipcode}/>
+                        <input required  type="text" placeholder={'Country'} name="country" onChange={onChangeHandler} value={data.country}/>
                     </div>
-                    <input type="text" placeholder={"phone"} name="phone" onChange={onChangeHandler} value={data.phone}/>
+                    <input  required type="text" placeholder={"phone"} name="phone" onChange={onChangeHandler} value={data.phone}/>
                 </div>
 
 
@@ -90,7 +108,7 @@ const PlaceOrder=()=>{
 
                         </div>
                         <button
-                            className={"cart-total-button border-none text-white bg-orange-600  mt-[30px]"} type="submit">PROCEED To
+                            className={"cart-total-button border-none text-white bg-orange-600  mt-[30px]"} type="submit" onClick={placeOrder}>PROCEED To
                             PAYMENT
                         </button>
                     </div>
